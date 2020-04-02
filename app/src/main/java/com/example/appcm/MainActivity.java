effefe453d6cbfb1f0c574da3a677fd59eefb1aa
lolilolii <47814316+lolilolii@users.noreply.github.com>
2020-04-02 16:19:35 +0200

    intento de menu

--- app/src/main/java/com/example/appcm/MainActivity.java
+++ app/src/main/java/com/example/appcm/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appcm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+    public boolean onMenuItemSelected(int featureId, MenuItem item){
+        switch (item.getItemId()){
+            case R.id.foto
+        }
     }
 }
